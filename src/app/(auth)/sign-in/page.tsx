297b96d358f85a1fb5808eb3336c5777f5a7c6e6
Amr Elskhawy <amrelskhawy@gmail.com>
2024-11-15 16:00:58 +0200

    start adding Sign in and sign up components in their pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+
+import SignInCard from '@/features/auth/components/sign-in-card'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+      <SignInCard />
   )
 }
 
