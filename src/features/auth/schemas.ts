46b88d3725b5b666f7e69e49df4b819b0f44359b
Amr Elskhawy <amrelskhawy@gmail.com>
2024-11-20 20:58:49 +0200

    adds registerSchema in schema file

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -4,3 +4,14 @@ export const loginSchema = z.object({
     email: z.string().trim().min(1, "Required").email(),
     password: z.string().min(8, "Minimum 8 Charachters")
 })
+
+export const registerSchema = z.object({
+    name: z.string().trim().min(1, "Required"),
+    email: z.string().trim().min(1, "Required").email(),
+    password: z.string().min(8, "Minimum 8 Charachters"),
+    confirm_password: z.string().min(8, "Minimum 8 Charachters")
+  }).refine((data) => data.password === data.confirm_password, {
+    message: "Passwords must match",
+    path: ["confirm_password"], // Specifies the path of the error
+  });
+  
